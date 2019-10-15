224426ec6a6d05463b8a32978505fce6b8e4c4fc
shashiranjan <sssepn@gmail.com>
2019-10-15 19:51:27 +0530

    admin panel

--- coworkapp/models.py
+++ coworkapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+# from djnago.conf import settings
 
-# Create your models here.
+# # # Create your models here.
+
+# User = settings.AUTH_USER_MODEL
+
+# class WorkingSpaceDetails(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     space_type = models.CharField(max_length = 200, null = True)
+#     seater = models.IntegerField( null = True)
+#     total_quantity = models.IntegerField( null = True)
+
+# class subscriptions(models.Model):
+#     userspace_id = models.ForeignKey(WorkingSpaceDetails, on_delete=models.CASCADE)
+#     time = models.IntegerField(primary_key=True )
+#     price = models.IntegerField(primary_key=True )
+
+
+
+
--- coworkingspace/settings.py
+++ coworkingspace/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'coworkapp.apps.CoworkappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -73,10 +74,26 @@ WSGI_APPLICATION = 'coworkingspace.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'coworkdb',
+#         'USER': 'coworkuser',
+#         'PASSWORD': 'mypassword',
+#         'HOST': '13.127.186.128',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mypsql',
+        'USER': 'mydatabaseuser',
+        'PASSWORD': 'mypassword',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -118,3 +135,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
