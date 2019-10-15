e4808e8da6072a98eb778ffc1855bfc24135b07d
shashiranjan <sssepn@gmail.com>
2019-10-15 14:13:54 +0530

    add coworkapp

---
+++ coworkapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CoworkappConfig(AppConfig):
+    name = 'coworkapp'
--- coworkingspace/settings.py
+++ coworkingspace/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,10 +21,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%2k*0xhd19!^yb#@rp7s2-%!ql71!zttm(o7bkl5%ti_gfi=hx'
-
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
