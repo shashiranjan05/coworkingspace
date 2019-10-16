d53f298600a0a612a912e6ddc8448e13b559406b
shashiranjan <sssepn@gmail.com>
2019-10-16 12:49:40 +0530

    added user login form

--- coworkingspace/settings.py
+++ coworkingspace/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -135,5 +136,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'index'
+
 
 
